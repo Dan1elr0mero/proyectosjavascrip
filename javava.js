387a31eb3d99310c94a17d0b280abbb4f4f8dd9e
Daniel Romero <romeroblessjunior@gmail.com>
2023-11-17 10:07:30 -0500

    pruebitas

---
+++ javava.js
@@ -0,0 +1,10 @@
+"use strig"
+
+    const nombre = prompt ("cual es tu nombre")
+if (nombre=Number && nombre=null) {
+    alert(no es tu nombre)
+} else {
+    alert("tu nombre es "nombre)
+}
+
+//saludar y preguntar el nombre si no responde poner hola desconocido 
